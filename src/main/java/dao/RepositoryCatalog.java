a6254c283bc9c302593b548c4eda5a32ed924bd1
Michal Kubiak <michal.kubiak@l.fancyfon.com>
2018-01-08 21:02:10 +0100

    Implemented User Repository Factory

--- src/main/java/dao/RepositoryCatalog.java
+++ src/main/java/dao/RepositoryCatalog.java
@@ -1,6 +1,10 @@
 package dao;
 
+import dao.mappers.BookResultMapper;
+import dao.mappers.LibraryResultMapper;
 import dao.mappers.UserResultMapper;
+import domain.Book;
+import domain.Library;
 import domain.User;
 
 import java.sql.Connection;
@@ -20,6 +24,26 @@ public class RepositoryCatalog {
     public Repository<User> users(){
         try {
             return new UserRepository(connection, new UserResultMapper());
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    public Repository<Book> books(){
+        try {
+            return new BookRepository(connection, new BookResultMapper());
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    public Repository<Library> libraries(){
+        try {
+            return new LibraryRepository(connection, new LibraryResultMapper());
 
         } catch (SQLException e) {
             e.printStackTrace();
